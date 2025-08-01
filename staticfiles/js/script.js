f4ea8ba5466dcc170f4bd442cef3d26b82a485ea
Webcaique <sidraocaique@gmail.com>
2025-08-01 15:41:12 -0300

    Update script.js

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -248,7 +248,7 @@ function randomBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//const socket = new WebSocket("ws://" + window.location.host + "/ws/leaderboard/");
+const socket = new WebSocket("ws://" + window.location.host + "/ws/leaderboard/");
 
 const csrfToken = window.csrfToken
 
